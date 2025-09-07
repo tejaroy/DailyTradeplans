d6b3ea5960e46b3276f8b45761c745e259243ea6
TEJA <teja.yangala@dvara.com>
2025-09-07 19:41:05 +0530

    code changes

--- stock_trades/settings.py
+++ stock_trades/settings.py
@@ -22,10 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-&2nlti%yxn_65wzoa6%n9(3q^g=^yw08v9c59cpvjpms80p(=_'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# SECURITY WARNING: don't run with debug turned on in production! 
 
-ALLOWED_HOSTS = []
+import os
+
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
